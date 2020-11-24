97ecd7f07f842754619dfe9c64c3b7ab3d561a3e
zhangzlyuyx <zhangzlyuyx@zhangzlyuyx.com>
2020-11-24 13:59:39 +0800

    create-centos-host.sh

--- create-centos-host.sh
+++ create-centos-host.sh
@@ -1,6 +1,5 @@
-@echo off
-set /p centosPort="input centos port:" 
-echo centos port:%centosPort%
-docker run -dit --name centos%centosPort% --hostname centos%centosPort% --net=host --restart=unless-stopped  -v /home/data/centos/%centosPort%:/usr/local/app --privileged=true zhangzlyuyx/centos76:1.0 /etc/rc.local
-docker logs centos%centosPort%
-@echo on
+read -t 30 -p "input centos port:" centosPort
+echo -e "\n"
+echo "centos port:$centosPort"
+docker run -dit --name centos$centosPort --hostname centos$centosPort --net=host --restart=unless-stopped  -v /home/data/centos/$centosPort:/usr/local/app --privileged=true zhangzlyuyx/centos76:1.0 /etc/rc.local
+docker logs centos$centosPort
